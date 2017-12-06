63765294512004ecf27b922cb5fbc2265e61275f
clarknoah <noahbc08@gmail.com>
2017-12-06 12:47:57 -0600

    added ANgular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,54 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 //services
-import {ApiService} from './service/api.service';
+//import {ApiService} from './service/api.service';
+
+//UI Modules
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ReactiveFormsModule} from '@angular/forms';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatAutocompleteModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatTableModule,
+  MatMenuModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatSelectModule,
+  MatStepperModule,
+  MatTabsModule} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
-    ApiService
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatTableModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatStepperModule,
+    MatTabsModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
